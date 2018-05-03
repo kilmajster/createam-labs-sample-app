0204d67cd388824a67f162e1be95f9650049424a
Łukasz Włódarczyk <lukasz.wlodarczyk@onegini.com>
2018-05-03 19:33:31 +0200

    mustache test

--- src/main/java/com/createam/sample/SampleAppApplication.java
+++ src/main/java/com/createam/sample/SampleAppApplication.java
@@ -1,22 +1,25 @@
 package com.createam.sample;
 
+import java.util.Date;
+import java.util.Map;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 @Controller
 public class SampleAppApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SampleAppApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(SampleAppApplication.class, args);
+  }
 
-	@RequestMapping("/")
-	public String index(final ModelMap modelMap) {
-		modelMap.addAttribute("message", "Hi there, it works! 🤘");
-		return "index";
-	}
+  @GetMapping("/")
+  public String displayGreeting(Map<String, Object> model) {
+    model.put("message", "Hi, it works!");
+    model.put("time", new Date());
+    return "index";
+  }
 }
